51ce6a8fd87324414c6e3109f88b3cc28cc541ee
Denis Melnik <frntprog@gmail.com>
2021-04-09 12:09:08 +0300

    deleted console logs

--- Backend/routes/todos.js
+++ Backend/routes/todos.js
@@ -1,56 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Todo = require('../models/Todo');
+const Todo = require("../models/Todo");
 
-router.get('/',  async (req,res) => {
-    console.log('Get')
-    try{
-        const todos = await Todo.find().sort({date: -1});
-        res.json(todos)
-    }catch (e) {
-        res.json({message: e})
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const todos = await Todo.find().sort({ date: -1 });
+    res.json(todos);
+  } catch (e) {
+    res.json({ message: e });
+  }
+});
 
-router.post('/',  async (req,res) => {
-    const todo = new Todo({
-        task: req.body.task
-    })
-    try{
-        const savedTodo = await todo.save();
-        res.json(savedTodo);
-    }catch (e) {
-        res.json({message: e})
-    }
-})
+router.post("/", async (req, res) => {
+  const todo = new Todo({
+    task: req.body.task,
+  });
+  try {
+    const savedTodo = await todo.save();
+    res.json(savedTodo);
+  } catch (e) {
+    res.json({ message: e });
+  }
+});
 
-router.delete('/one/:todoID', async (req, res) => {
-    try {
-        const removedTodo = await Todo.deleteOne({_id: req.params.todoID})
-        res.json(removedTodo);
-    } catch (e) {
-        res.json({message: e})
-    }
-})
+router.delete("/one/:todoID", async (req, res) => {
+  try {
+    const removedTodo = await Todo.deleteOne({ _id: req.params.todoID });
+    res.json(removedTodo);
+  } catch (e) {
+    res.json({ message: e });
+  }
+});
 
-router.delete('/done', async (req, res) => {
-        try {
-            const removedTodo = await Todo.deleteMany({completed: true})
-            res.json(removedTodo);
-        } catch (e) {
-            res.json({message: e})
-        }
-    }
-)
+router.delete("/done", async (req, res) => {
+  try {
+    const removedTodo = await Todo.deleteMany({ completed: true });
+    res.json(removedTodo);
+  } catch (e) {
+    res.json({ message: e });
+  }
+});
 
-router.patch('/:todoID', async (req, res)=> {
-    try{
-        const updatedTodo =  await Todo.updateOne({_id: req.params.todoID}, {$set: {completed: req.body.completed}})
-        res.json(updatedTodo);
-        console.log('Patch')
-    }catch (e) {
-        res.json({message: e})
-    }
-})
+router.patch("/:todoID", async (req, res) => {
+  try {
+    const updatedTodo = await Todo.updateOne(
+      { _id: req.params.todoID },
+      { $set: { completed: req.body.completed } }
+    );
+    res.json(updatedTodo);
+  } catch (e) {
+    res.json({ message: e });
+  }
+});
 
 module.exports = router;
