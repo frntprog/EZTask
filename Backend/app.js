75ce369557a7519650cbc2c84a688308c4556489
Melnyk Denys <dmelnyk@exadel.net>
2021-05-06 11:34:10 +0300

    Added login/registration controller

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authRouter = require('./routes/authRouter');
 require('dotenv/config');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 const todoRoute = require('./routes/todos');
@@ -13,7 +16,10 @@ const subTaskRoute = require('./routes/SubTasks');
 app.use(cors());
 app.use('/todo', todoRoute);
 app.use('/subtask', subTaskRoute);
+app.use('/auth', authRouter);
 
-mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true});
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true
+});
 
 app.listen(3000);
